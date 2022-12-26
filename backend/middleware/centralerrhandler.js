bc68b44a89030f20548befafd3d27014d4696c04
Avishay <avishaypesk@gmail.com>
2022-12-27 00:10:45 +0200

    fix err handler

--- backend/middleware/centralerrhandler.js
+++ backend/middleware/centralerrhandler.js
@@ -1,6 +1,6 @@
 const { INTERNAL_SERVER } = require('../utils/errorcodes');
 
-module.exports = (err, req, res) => {
+module.exports = (err, req, res, next) => {
   console.log('caught by central err handler\n', err);
 
   const { statusCode = INTERNAL_SERVER, message } = err;
